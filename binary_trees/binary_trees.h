ccd31b0e78de16166d6a726f0b0d012d14792355
ebitech02 <ebipresh02@gmail.com>
2024-06-06 14:25:44 -0700

    new

--- binary_trees/binary_trees.h
+++ binary_trees/binary_trees.h
@@ -3,9 +3,9 @@
 #define _BINARY_TREES_H_
 
 #include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include <limits.h>
 
 /**
@@ -22,9 +22,11 @@ struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-};
-
+}; 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
