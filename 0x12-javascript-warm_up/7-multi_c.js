af54dfb81e6138921e26e42ea46baf989cf4281c
ebitech02 <ebipresh02@gmail.com>
2024-06-19 23:32:27 +0100

    initial commit

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2])) {
+if (isNaN(Number(process.argv[2]))) {
   console.log('Missing number of occurrences');
 } else {
-  for (let i = 0; i < process.argv[2]; i++) {
+  for (let i = 0; i < Number(process.argv[2]); i++) {
     console.log('C is fun');
   }
 }
