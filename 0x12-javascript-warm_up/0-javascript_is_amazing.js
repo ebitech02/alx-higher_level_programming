e6302f0769136be9eac14d8f95c52911ff9d3ec4
ebitech02 <ebipresh02@gmail.com>
2024-06-19 16:04:55 +0100

    new commit

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-
+/*
+ * declare and initialize a constant variable called myVar
+ * print the value of myVar to output console
+ */
 const myVar = 'Javascript is amazing';
 console.log(myVar);
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-
+/*
+ * prints to output console the texts in three lines
+ */
 console.log('C is fun');
 console.log('Python is cool');
 console.log('Javascript is amazing');
