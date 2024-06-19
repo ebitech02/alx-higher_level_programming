c98abb5c04fd1e97f8f05a5dd794cbf0b4136444
ebitech02 <ebipresh02@gmail.com>
2024-06-19 14:25:06 -0700

    new

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-/*
- * declare and initialize a constant variable called myVar print the value of myVar to output console
- */
-
 const myVar = 'Javascript is amazing';
-
 console.log(myVar);
