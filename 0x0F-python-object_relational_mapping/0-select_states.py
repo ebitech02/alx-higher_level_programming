1523ccc37c266410254c99baa8cd926c5c6cd51e
ebitech02 <ebipresh02@gmail.com>
2024-06-23 11:47:09 -0700

    new commit

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 """
-a script that lists all states from the database  hbtn_0e_0_usa
+script lists all states from the
+database `hbtn_0e_0_usa`
 """
-import  MySQLdb
-import sys
 
-if __name__ == "__main__":
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
     """
-    retrieve the MySQL username, password and database from the command line arguments
+    Access to the database and get the states
+    from the database.
+    connect to database
     """
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    """
-    Access the database and connect to the database
-    """
-    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username, passwd=mysql_password, db=database_name)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
-    for row in rows:
+    db_connect = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+    cursor = db_connect.cursor()
+
+    cursor.execute("SELECT * FROM states")
+
+    rows_selected = cursor.fetchall()
+
+    for row in rows_selected:
         print(row)
-    cursor.close()
-    db.close()
-
