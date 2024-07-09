0df4758eb69090fd982f0ff0fdc96a325c482cf8
ebitech02 <ebipresh02@gmail.com>
2024-07-09 14:44:11 -0700

    testing

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,11 @@
 # using the python package urllib to fetch a resource
 import urllib.request
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         body = response.read()
         print("Body response:")
         print(f"\t- type: {type(body)}")
         print(f"\t- content: {body}")
         print(f"\t- utf8 content: {body.decode('utf-8')}")
+
--- 0x11-python-network_1/README.md
+++ 0x11-python-network_1/README.md
@@ -4,3 +4,4 @@ Python for backend using the package urllib
 Objectives -
 How to fetch resources over a network
 How to handle HTTP requests and responses
+
