7ffe63152e512d017500a30194429f3c48d968c1
ebitech02 <ebipresh02@gmail.com>
2024-07-09 11:59:43 -0700

    intial code

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# using the python package urllib to fetch a resource
+import urllib.request
+with.urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    body = response.read()
+print("Body response:")
+print(f"\t- type: {type(body)}"
+print(f"\t- content: {body}")
+print(f"\t- utf8 content: {body.decode('utf-8')}"
