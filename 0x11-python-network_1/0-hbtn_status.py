83df2703e30d46e50f06e2697dd452cca9b101a5
ebitech02 <ebipresh02@gmail.com>
2024-07-09 15:26:23 -0700

    final code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-# using the python package urllib to fetch a resource
+# using the python package urllib to fetch resources
 import urllib.request
-
+"""
+this module is used to handle urls (http)
+"""
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
         body = response.read()
         print("Body response:")
         print(f"\t- type: {type(body)}")
         print(f"\t- content: {body}")
         print(f"\t- utf8 content: {body.decode('utf-8')}")
-
